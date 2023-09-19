625aeb8d4b57a645598a78d1984ebc11b27a2416
thi4go-git <thi4go19@gmail.com>
2023-09-19 11:43:05 -0300

    Configurações

--- producer/src/main/java/com/dynns/cloudtecnologia/producer/service/MessageServiceImpl.java
+++ producer/src/main/java/com/dynns/cloudtecnologia/producer/service/MessageServiceImpl.java
@@ -1,23 +1,35 @@
 package com.dynns.cloudtecnologia.producer.service;
 
+import lombok.extern.log4j.Log4j2;
 import model.dto.MessageDTO;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 
+import java.util.function.BiConsumer;
+
 @Service
+@Log4j2
 public class MessageServiceImpl implements MessageService {
     @Autowired
     private KafkaTemplate<String, Object> kafkaTemplate;
 
-    private static final Logger LOG = LoggerFactory.getLogger(MessageServiceImpl.class);
-
     @Override
     public void createEvent(MessageDTO messageDTO) {
         MessageDTO mensagemKafka = new MessageDTO(messageDTO.getStatus(), messageDTO.getDescricao(), messageDTO.getTopico());
-        kafkaTemplate.send(mensagemKafka.getTopico(), mensagemKafka);
-        LOG.info("Mensagem enviada para o KAFKA! Payload: " + mensagemKafka.toString());
+        log.info("Enviando para o KAFKA... Payload: " + mensagemKafka.toString());
+        kafkaTemplate.send(mensagemKafka.getTopico(), messageDTO)
+                .whenComplete(getResultSendMessage());
+    }
+
+    private static BiConsumer<SendResult<String, Object>, Throwable> getResultSendMessage() {
+        return (success, ex) -> {
+            if (ex != null) {
+                log.error("::: Erro ao enviar Mensagem KAFKA :::");
+            } else {
+                log.info("::: Sucesso ao enviar Mensagem KAFKA :::");
+            }
+        };
     }
 }
